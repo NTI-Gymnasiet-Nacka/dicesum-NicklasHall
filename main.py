eb580dc03671b44a1db582d45d0e8fa0db4755f9
NicklasHall <nicklas.hall@elev.ga.ntig.se>
2024-08-21 11:08:14 +0200

    dice

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # Dice sum probability calculator
-# Författare: 
-# Datum:
+# Författare: Nicklas Hall      
+# Datum: 2024-08-21
+from collections import Counter
 
+all_sum = []
 def main():
     user_input = input().split(" ")
+    for i in range (int(user_input[0])+1):
+        for x in range(int(user_input[1])+1):
+             summ = x + i  
+             all_sum.append(summ)
+        most_common = Counter(all_sum).most_common(5)
+        print(most_common[0])
+
 
 if __name__ == "__main__":
-    main()
+      main()
+   
